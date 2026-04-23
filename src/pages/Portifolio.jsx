cd52110d866d5abdcd8a89f54dd340b2d3e26b53
Lkzin0122 <bitencourtes@gmail.com>
2026-04-23 19:05:44 -0300

    Remodelação do portifolio

--- src/pages/Portifolio.jsx
+++ src/pages/Portifolio.jsx
@@ -1,32 +1,37 @@
-import './App.css'
-import todoListImg from './assets/imgs/ToDo List.png'
-import safeSolImg from './assets/imgs/SafeSol.png'
-import Curriculo from './assets/pdf/Curriculo_Lucas.pdf?url'
+import '../pages/App.css'
+import todoListImg from '../assets/imgs/ToDo List.png'
+import safeSolImg from '../assets/imgs/SafeSol.png'
+import Curriculo from '../assets/pdf/Curriculo_Lucas.pdf?url'
 
 function Portifolio() {
   return (  
     <div className="container">
-      <h1 className='Title'>Olá me chamo <br /> Lucas Bitencourt</h1>
-      <p className='subtitle'>Técnico em Informática | Desenvolvedor Full Stack Jr | Java • React • Spring Boot • Flutter • SQL</p>
-      
-      <div className="contact-buttons">
-        <a className="contact-btn Curriculo" href={Curriculo} download target="_blank" rel="noopener noreferrer">Currículo</a>
-        <a className="contact-btn linkedin" href="https://www.linkedin.com/in/bitencourtlucas/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-        <a className="contact-btn github" href="https://github.com/Lkzin0122" target="_blank" rel="noopener noreferrer">GitHub</a>
-      </div>
 
       <a className="github-link" href="https://github.com/Lkzin0122">
       <img className="github-image" src="https://github.com/Lkzin0122.png" alt="Foto Lucas Bitencourt" />
       </a>
+
+      <h1 className='Title'>Olá me chamo <br /> Lucas Bitencourt</h1>
+      <p className='subtitle'>Técnico em Informática | Desenvolvedor Full Stack Jr | Java • React • Spring Boot • Flutter • SQL</p>
+      
+      <div className="contact-buttons">
+        <a className="contact-btn-Curriculo" href={Curriculo} download target="_blank" rel="noopener noreferrer">Currículo</a>
+        <a className="contact-btn-linkedin" href="https://www.linkedin.com/in/bitencourtlucas/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a className="contact-btn-github" href="https://github.com/Lkzin0122" target="_blank" rel="noopener noreferrer">GitHub</a>
+      </div>
+
+     
       
       <h2 className="about-title">Sobre mim</h2>
       
      <div className="About">
       <p >Olá me chamo Lucas Bitencourt, tenho 18 anos, sou de São Paulo e possuo curso Técnico em 
-        Informática pelo ITB. Sou um entusiasta de tecnologia e programação, com experiência prática
+        Informática pela instituição FIEB. Sou um entusiasta de tecnologia e programação, com experiência prática
         no desenvolvimento de aplicações front-end, back-end e mobile. Durante minha formação em 
         Técnico em Informática, tive contato direto com programação de verdade, trabalhando com linguagens, 
-        frameworks e conceitos usados no mercado de trabalho. <p>Além disso, participei de projetos práticos 
+        frameworks e conceitos usados no mercado de trabalho. 
+        
+        <p>Além disso, participei de projetos práticos 
         que me permitiram aplicar meus conhecimentos em situações reais, desenvolvendo habilidades de resolução 
         de problemas e trabalho em equipe.</p> Estou sempre buscando aprender novas tecnologias e aprimorar minhas 
         habilidades para me tornar um profissional cada vez mais completo e preparado para os desafios do mercado 
@@ -90,7 +95,7 @@ function Portifolio() {
       </div>
      
       <div className="projects-section">
-        <h2>Projetos</h2>
+        <h2>Projetos Desenvolvidos</h2>
         <div className="projects-grid">
           <div className="project-item">
             <a href="https://github.com/Lkzin0122/safe_solutions_flutter">
